a90f2fc4a53aaf77e7aa14b4700169b23930dc79
Samreen Bano <sameeen.2005@gmail.com>
2026-07-02 10:27:51 +0530

    function basics

---
+++ function.java
@@ -0,0 +1,15 @@
+//print the name using function
+// import java.util.Scanner;
+
+// public class function {
+//     public static void printMyName(String name){
+//         System.out.println(name);
+//         return;
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String name = sc.next();
+//         printMyName(name);  //here we are calling the function and passing the name as an argument
+//     }
+// }
