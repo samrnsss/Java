efb77c0571a30822e78c43bee880316fbb82d004
Samreen Bano <sameeen.2005@gmail.com>
2026-07-02 12:08:39 +0530

    solved one qs on functions

--- function.java
+++ function.java
@@ -32,3 +32,26 @@
 //     }
 // }
 
+// //print the factorial of a number using function
+// import java.util.Scanner;
+
+// public class function {
+//     public static void calculateFactorial(int n){
+//         if(n<0){
+//             System.out.println("Factorial is not defined for negative numbers.");
+//             return;
+//         }
+//         int factorial = 1;
+//         for(int i = n; i >= 1; i--){
+//         factorial = factorial * i;
+//     }
+//         System.out.println( factorial);
+//         return;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         calculateFactorial(n);  //calling the factorial function
+//     }
+// }
+        
---
+++ practiceQs.java
@@ -0,0 +1,19 @@
+//1. Enter 3 numbers from the user and make a function to print their average.
+// import java.util.Scanner;
+// public class practiceQs {
+//     public static double  averageOfThree(double a, double b, double c){
+//         double average = (a+b+c)/3;
+//         System.out.print("The average is: " + average);
+//         return average;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter 3 numbers: ");
+//         double a = sc.nextDouble(); //pass the values to the function
+//         double b = sc.nextDouble();
+//         double c = sc.nextDouble();
+//         double result = averageOfThree(a,b,c);
+//         System.out.println("The returned average is: " + result);
+//     }
+
+// }
