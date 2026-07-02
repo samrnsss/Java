1c56fd3fbd03dcc7610b8ef04d9ea8d0537be5f3
Samreen Bano <sameeen.2005@gmail.com>
2026-07-02 10:41:08 +0530

    advance pattern problems

--- function.java
+++ function.java
@@ -13,3 +13,22 @@
 //         printMyName(name);  //here we are calling the function and passing the name as an argument
 //     }
 // }
+
+//print the  sum of two numbers using function
+// import java.util.Scanner;public class function {
+//     public static int calculateSum(int a, int b){
+//         int sum = a + b;
+//         return sum;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter first number: ");
+//         int a = sc.nextInt();
+//         System.out.print("Enter second number: ");
+//         int b = sc.nextInt();
+        
+//         int sum = calculateSum(a, b);  //calling the sum function
+//         System.out.println("The sum is: " + sum);
+//     }
+// }
+
