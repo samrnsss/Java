44213fbab0c35399e04ec7a6131fa5664b5f69b5
Samreen Bano <sameeen.2005@gmail.com>
2026-07-08 01:21:55 +0530

    array basics

---
+++ Array.java
@@ -0,0 +1,106 @@
+
+// public class Array {
+//     public static void main(String args[]){
+//         int[] marks = new int [3];
+//         marks[0] = 97;
+//         marks[1] = 98;
+//         marks[2] = 95;
+//         System.out.println(marks[0]);
+//         System.out.println(marks[1]);
+//         System.out.println(marks[2]);
+//     }
+// }
+
+
+
+// public class Array {
+//     public static void main(String args[]){
+//         int[] marks = new int[3];
+//         marks[0] = 97;
+//         marks[1] = 98;
+//         marks[2] = 95;
+//         for(int i = 0; i < 3; i++){
+//             System.out.println(marks[i]);
+//         }
+//     }
+// }
+
+
+
+
+// public class Array {
+//     public static void main(String args[]){
+//         int[] marks = new int[3];
+//         marks[0] = 97;
+//         marks[1] = 98;
+//         marks[2] = 95;
+//         for(int i = 0; i < 3; i++){
+//             System.out.println(marks[i]);
+//         }
+//     }
+// }
+
+
+
+// public class Array{
+//     public static void main(String args[]){
+//         int marks[] = {97, 98, 95};
+//         for(int i = 0; i < 3; i++){
+//             System.out.println(marks[i]);
+//         }
+//     }
+// }
+
+
+// }  
+
+
+
+
+// import java.util.Scanner;
+// public class Array{
+//     public static void main(String args[]){
+
+//         Scanner sc = new Scanner(System.in);
+//         int size = sc.nextInt();
+//         int numbers[] = new int[size];
+//             //input 
+//         for(int i = 0; i < size; i++){
+//             numbers[i] = sc.nextInt();
+//         }
+//         for(int i = 0; i < size; i++){
+//             System.out.println(numbers[i]);
+//         }
+//     }
+// }
+
+
+
+// qs-take an array as input from the user. Search for a given number x and print the index at which it occurs.
+//linear search
+// import java.util.Scanner;
+// public class Array{
+//     public static void main(String args[]){
+
+//         Scanner sc = new Scanner(System.in);
+//         int size = sc.nextInt();
+//         int numbers[] = new int[size];
+
+//             //input 
+//         for(int i = 0; i < size; i++){
+//             numbers[i] = sc.nextInt();
+//         }
+
+//         int x = sc.nextInt();
+
+//         //output
+//         for(int i = 0; i <numbers.length; i++){
+//             if(numbers[i] == x){
+//                 System.out.println("x found at index " + i);
+//             }
+//         }
+
+//     }
+// }
+
+// //size = array.length
