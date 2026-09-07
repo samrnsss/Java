210cafa716881ff1d7607c0781a547b33223979d
Samreen Bano <sameeen.2005@gmail.com>
2026-09-07 18:34:35 +0530

    float value returned

---
+++ AvgOfThreeNum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class AvgOfThreeNum{
+    public static float avgOfThreeNum(int a, int b , int c){
+        float avg = (a+b+c)/3;
+        return avg;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter three numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        float avg = avgOfThreeNum(a,b,c);
+        System.out.println("Average of three numbers is: "+avg);
+    }
+}
