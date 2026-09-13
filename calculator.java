9e0f9f6d0f5d34e7e83375a35e723235a3300116
Samreen Bano <samreen22@gmail.com>
2026-09-13 12:33:20 +0530

    added modulo

--- calculator.java
+++ calculator.java
@@ -2,14 +2,16 @@ import java.util.Scanner;
 
 public class calculator {
     public static void main (String [] args){
+        
         Scanner sc = new Scanner(System.in);
         System.out.println ("enter the first number:");
         double num1 = sc.nextDouble();  //double is used to store decimal values
         System.out.println("enter the second number:");
         double num2 = sc.nextDouble();
         double result = 0; //to store the result of the calculation
-        System.out.println("enter the operator (+, -, *, /):");
+        System.out.println("enter the operator (+, -, *, /, %):");
         char op = sc.next().charAt(0); //to read the operator as a
+
         switch(op){
             case '+': 
                 result = num1 + num2;
@@ -33,6 +35,15 @@ public class calculator {
             case '/':
                 if (num2 != 0) {
                 result = num1 / num2;
+                System.out.println("result: " + result);
+                } else {
+                System.out.println("Error: Division by zero is not allowed.");
+            } 
+                break;
+
+            case '%':
+                if (num2 != 0) {
+                result = num1 % num2;
                 System.out.println("result: " + result);
                 } else {
                 System.out.println("Error: Division by zero is not allowed.");
