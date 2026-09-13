f1e4d15c6467b58a901aab5b5fd90094170d4572
Samreen Bano <samreen22@gmail.com>
2026-09-13 14:38:25 +0530

    reverse a given number using modulo

--- reverse.java
+++ reverse.java
@@ -1,10 +1,26 @@
 
-public class reverse {
+// public class reverse {
+//     public static void main(String [] args){
+//         int n = 1234;
+//         for ( int i = n; i > 0; i = i / 10){
+//             int lastdigit = i % 10;
+//             System.out.print(lastdigit);
+//         }
+//     }
+// }
+
+
+//reverse the given number
+
+public class reverse{
     public static void main(String [] args){
-        int n = 1234;
-        for ( int i = n; i > 0; i = i / 10){
-            int lastdigit = i % 10;
-            System.out.print(lastdigit);
+        int n = 987654321;
+        int rev = 0;
+        while(n > 0){
+            int lastdigit = n % 10;
+            rev = rev * 10 + lastdigit;
+            n = n / 10;
         }
+        System.out.println(rev);
     }
 }
